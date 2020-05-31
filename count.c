dd5bdb171a114a66cbe86248fc52f70fed789b26
prathu47 <53473144+prathu47@users.noreply.github.com>
2020-05-31 12:41:57 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,56 @@
+ Write a C Program to count Uppercase, Lowercase, special character and numeric values for a given String
+ Input : Read a String (combination of character, numeric, special character) from keyboard.
+Ex: #HaaGs01fOr@haaks07
+Output :
+Upper case letters : 5
+Lower case letters : 8
+Numbers : 4
+Special Characters : 2
+
+
+#include<string.h>
+#define MAX_SIZE 100 
+
+
+int main()
+{
+    char str[MAX_SIZE];
+    int lower,upper,number,special, i;
+
+    lower=upper=number=special = i = 0;
+
+
+
+    printf("Enter any string : ");
+    gets(str);
+
+
+    while(str[i]!='\0')
+    {
+        if(str[i]>='a' && str[i]<='z')
+        {
+            lower++;
+        }
+        else if(str[i]>='A' && str[i]<='Z')
+        {
+            upper++;
+        }
+        else if(str[i]>='0' && str[i]<='9')
+        {
+            number++;
+        }
+        else
+        {
+            special++;
+        }
+
+        i++;
+    }
+
+    printf("Lower case letters = %d\n", lower);
+    printf("Uppercase letters = %d\n", upper);
+    printf("Numbers = %d\n", number);
+    printf("Special characters = %d", special);
+
+    return 0;
+}
