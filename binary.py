66587c36d03d7a4e2ff1f05f736b81d74a31a68e
prathu47 <53473144+prathu47@users.noreply.github.com>
2020-06-19 10:35:31 +0530

    16th june

---
+++ binary.py
@@ -0,0 +1,32 @@
+# Python implementation to check if 
+# given Binary tree is a BST or not 
+class Node: 
+	def __init__(self, val): 
+		self.data = val 
+		self.left = None
+		self.right = None
+prev = None
+def isbst(root): 
+	global prev 
+	prev = None
+	return isbst_rec(root) 
+def isbst_rec(root): 
+	global prev 
+	if root is None: 
+		return True
+	if isbst_rec(root.left) is False: 
+		return False
+	if prev is not None and prev.data > root.data: 
+		return False
+	prev = root 
+	return isbst_rec(root.right) 
+root = Node(4) 
+root.left = Node(2) 
+root.right = Node(5) 
+root.left.left = Node(1) 
+root.left.right = Node(3) 
+
+if isbst(root): 
+	print("is BST") 
+else: 
+	print("not a BST") 
